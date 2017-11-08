119b112137295fc7862cf609dac9b792973a4faf
Emily Parkes <emilycoco@me.com>
2017-11-08 17:42:08 +1300

    trying again add totals

--- dom-events-and-classes/game.js
+++ dom-events-and-classes/game.js
@@ -45,14 +45,15 @@ function updateCounts () {
   let dots = document.getElementsByClassName('board')[0].children)
 
   for (let i = 0; i < dots.length; i++) {
-    if (dots[i].classList.contains('blue')) {
-      totals.blue++
-    } else if (dots[i].classList.contains('green')){
-      totals.green++
-    } else if (dots[i].classList.contains('invisible')) {
-      totals.invsible++
-    }
+  if (dots[i].classList.contains('blue')) {
+    totals.blue++
+  } else if (dots[i].classList.contains('green')) {
+    totals.green++
+  } else if (dots[i].classList.contains('invisible')) {
+    totals.invisible++
   }
+}
+
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
